32754d68aa802d027c6d815b534da92de58747ca
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-10-02 14:56:56 -0400

    building post for api and server

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.use(bodyParser.json());
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
+var friends = require("./app/data/friends.js");
+
+app.post("/api/friends", function(req, res) {
+
+    var newFriend = req.body;
+  
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+  
+    friends.push(newFriend);
+  
+    res.json(newFriend);
+  });
 
 
 app.listen(PORT, function () {
