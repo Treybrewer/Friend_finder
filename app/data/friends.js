2f88447a212fe15a4d327a229517e0b75a02cfe2
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-10-02 00:44:45 -0400

    building friends api

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,74 +1,74 @@
 
 var friends = [
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: ""
+        routeName: "chuck",
+        name: "Chuck Norris",
+        age: 78,
+        bio: "The show Survivor had the original premise of putting people on an island with Chuck Norris. There were no survivors, and nobody is brave enough to go to the island to retrieve the footage.",
+        image: "https://i.kym-cdn.com/photos/images/original/001/345/499/0f6.jpg"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: "",
+        routeName: "arnold",
+        name: "Arnold Schwarzenegger",
+        age: 71,
+        bio: "Come with me if you want to live!",
+        image: "http://i.dawn.com/large/2018/03/5a9e7e7d46339.jpg",
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: ""
+        routeName: "sylvester",
+        name: "Sylvester Stallone",
+        age: 72,
+        bio: "Every time I’ve failed, people had me out for the count, but I always come back.",
+        image: "https://www.grittv.com/wp-content/uploads/2014/09/Sylvester-Stallone-Expendables-a.jpg"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: ""
+        routeName: "clint",
+        name: "Clint Eastwood",
+        age: 88,
+        bio: "A hand out is what you get from the government, a hand up is what you get from a friend.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVJ7eIc9F91gnUYORuLTO9iNpBF9SBoPzjYLK9g1d-WY83SLZG"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: ""
+        routeName: "john",
+        name: "John Wayne",
+        age: 72,
+        bio: "Get off your horse and drink your milk.",
+        image: "https://s.yimg.com/ny/api/res/1.2/iK3f25CLCn_qgYvHxJE2EA--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAwO2lsPXBsYW5l/http://media.zenfs.com/en-US/blogs/movie-news/50959410-b6b6-11e3-8488-d9ba0443154b_jwayne.jpg"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: ""
+        routeName: "steve",
+        name: "Steve Austin",
+        age: 53,
+        bio: "Tune in next week, same Stone Cold time, same Stone Cold Channel!",
+        image: "https://vignette.wikia.nocookie.net/p__/images/9/97/Austin-0.jpg/revision/latest?cb=20160820203831&path-prefix=protagonist"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "", 
-        image: ""
+        routeName: "brock",
+        name: "Brock Lesner",
+        age: 41,
+        bio: "I don’t feel pain. I ain’t got time for that. I let other people feel pain for me.",
+        image: "https://130513-375933-1-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2016/07/Brock-Lesnar.jpg"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        iamge: ""
+        routeName: "mel",
+        name: "Mel Gibson",
+        age: 62,
+        bio: "If you're going to wear three hats, you'd better grow two more heads.",
+        iamge: "https://short-biography.com/wp-content/uploads/mel-gibson/Mel-Gibson.jpg"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: ""
+        routeName: "terry",
+        name: "Terry Crews",
+        age: 50,
+        bio: "Old Spice Body Spray will make you smell like power!",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT9xAFGckhQXMCVpznCD04WTJGPIiZ0TesPG2VM8ikgQfJDWI25Q"
     },
     {
-        routeName: "",
-        name: "",
-        age: "",
-        bio: "",
-        image: ""
+        routeName: "denzel",
+        name: "Denzel Washington",
+        age: 63,
+        bio: "We didn't land on Plymouth Rock. Plymouth Rock landed on us!",
+        image: "https://thenypost.files.wordpress.com/2016/12/608558788.jpg?quality=90&strip=all&w=618&h=410&crop=1"
     },
 ];
 
