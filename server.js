5ae242c95811fb191e93af329d39f794079123e8
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-10-09 16:13:11 -0400

    adding match cases

--- server.js
+++ server.js
@@ -27,21 +27,25 @@ app.post("/api/friends", function (req, res) {
 
     var newArray = [];
     var subtractionArray = [];
+    var finalResult = [];
     res.json(newFriend);
 
     for (var i = 0; i < 10; i++) {
         newArray.push(parseInt(newFriend.answer[i]));
     };
-    
+
     function Subtract(index, name) {
+        this.ran = false;
         this.index = index;
         this.name = name;
-        this.run = function() {
-            for(var i = 0; i < 10; i++) {
+        this.run = function () {
+            for (var i = 0; i < 10; i++) {
                 var operation = newArray[i] - this.index.answer[i];
+
                 subtractionArray.push(Math.abs(operation));
                 if (subtractionArray.length === 10) {
                     var subtractResult = subtractionArray[0] + subtractionArray[1] + subtractionArray[2] + subtractionArray[3] + subtractionArray[4] + subtractionArray[5] + subtractionArray[6] + subtractionArray[7] + subtractionArray[8] + subtractionArray[9];
+                    finalResult.push(subtractResult);
                     console.log(`
                     *************** New Friend Array ******************\n
                                     name: ${newFriend.name}\n
@@ -62,18 +66,70 @@ app.post("/api/friends", function (req, res) {
                     *************** ${newFriend.name} and ${this.name} Differences *******\n
                                     ${subtractResult}\n
                     **************************************************`);
-                };
-            };
-        };
-        
-      
-       
-    };
+                    subtractionArray = [];
 
-    var chuck = new Subtract(friends[0], "Chuck");
-    chuck.run();
-    var arnold = new Subtract(friends[1], "Arnold");
-    arnold.run();
+                };
+
+            };
+
+        };
+
+
+
+    };
+    switch (finalResult.length) {
+        case 0:
+            var chuck = new Subtract(friends[0], "Chuck");
+            chuck.run();
+
+        case 1:
+            var arnold = new Subtract(friends[1], "Arnold");
+            arnold.run();
+
+        case 2:
+            var sylvester = new Subtract(friends[2], "Sylvester");
+            sylvester.run();
+
+        case 3:
+            var clint = new Subtract(friends[3], "Clint");
+            clint.run();
+
+        case 4:
+            var john = new Subtract(friends[4], "John");
+            john.run();
+
+        case 5:
+            var steve = new Subtract(friends[5], "Steve");
+            steve.run();
+
+        case 6:
+            var brock = new Subtract(friends[6], "Brock");
+            brock.run();
+
+        case 7:
+            var mel = new Subtract(friends[7], "Mel");
+            mel.run();
+
+        case 8:
+            var terry = new Subtract(friends[8], "Terry");
+            terry.run();
+
+        case 9:
+            var denzel = new Subtract(friends[9], "Denzel");
+            denzel.run();
+
+
+    };
+    console.log(finalResult);
+    var match = Math.min(...finalResult);
+    console.log(match);
+    var matchIndex = finalResult.indexOf(match);
+    console.log(matchIndex);
+
+
+
+
+
 
 });
 
